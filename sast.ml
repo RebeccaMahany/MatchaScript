55f929d92ba670072393ea610aa8964c1b83fad0
Kimberly Hou <kimberlyjhou@gmail.com>
2017-05-04 11:21:11 -0400

    discussed changes

--- sast.ml
+++ sast.ml
@@ -1,68 +1,27 @@
 open Ast
 
-<<<<<<< HEAD
-type sexpr = 
-	  SIntLit of int
-=======
 type sbind = typ * string
 
 type svdecl = typ * string * expr
 
 type sexpr = 
     SIntLit of int
->>>>>>> ae7d5340e051e25abd64bef95f8885fcdbc92b6d
 	| SFloatLit of float
 	| SBoolLit of bool
 	| SCharLit of char
 	| SStringLit of string
-<<<<<<< HEAD
-(*	| SFunExpr of sfexpr *)
-=======
 	| SFunExpr of sfexpr
->>>>>>> ae7d5340e051e25abd64bef95f8885fcdbc92b6d
 	| SId of string * typ
 	| SBinop of sexpr * op * sexpr * typ
 	| SUnop of uop * sexpr * typ
 	| SAssign of sexpr * sexpr * typ
-<<<<<<< HEAD
-=======
 	| SObjCreate of string * sexpr list * typ
 	| SObjAccess of sexpr * sexpr * typ
 	(* Array access *)
->>>>>>> ae7d5340e051e25abd64bef95f8885fcdbc92b6d
 	| SCallExpr of sexpr * sexpr list * typ 
 	| SNoexpr
 
 and sstmt = 
-<<<<<<< HEAD
-	  SBlock of sstmt list
-	| SExpr of sexpr * typ
-	| SDeclStmt of typ * string * sexpr * typ 
-	| SFDeclStmt of sfdecl * typ 
-	| SReturn of sexpr * typ
-	| SIf of sexpr * sstmt * sstmt
-	| SFor of sexpr * sexpr * sexpr * sstmt
-	| SWhile of sexpr * sstmt
-
-(*TODO: Add support for sfexpr *)
-
-and sfdecl = {
-	sfdReturnType : typ;
-	sfdFname : string;
-	sfdFormals : bind list;
-	sfdBody : sstmt list; (* changing from ast *)
-}
-
-and sconstructs = {
-	sstmts: sstmt list;
-	sfdecls: sfdecl list; (* separating out for codegen *)
-}
-
-(*TODO: Add support for classes if necessary *)
-
-type sprogram = sconstructs
-
-=======
     SBlock of sstmt list
   | SExprStmt of sexpr
   | SVarDecl of svdecl
@@ -82,7 +41,9 @@ and sfexpr = {
 and sfdecl = {
   sfdReturnType : typ;
   sfdFname : string;
+  sfdParent : string;
   sfdFormals : bind list;
+  sfdLocals : expr list;
   sfdBody : stmt list;
 }
 
@@ -90,4 +51,7 @@ and scdecl = {
   scname : string;
   scproperties : svdecl list;
 }
->>>>>>> ae7d5340e051e25abd64bef95f8885fcdbc92b6d
+
+and sprogram = fdecl
+
+
