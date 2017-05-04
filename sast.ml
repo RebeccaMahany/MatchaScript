c6ea2dcda77c270ac117a66c7529c99c346d7fc8
Kimberly Hou <kimberlyjhou@gmail.com>
2017-05-04 12:53:51 -0400

    commented out sast

--- sast.ml
+++ sast.ml
@@ -10,13 +10,13 @@ type sexpr =
 	| SBoolLit of bool
 	| SCharLit of char
 	| SStringLit of string
-	| SFunExpr of sfexpr
+(*	| SFunExpr of sfexpr *)
 	| SId of string * typ
 	| SBinop of sexpr * op * sexpr * typ
 	| SUnop of uop * sexpr * typ
 	| SAssign of sexpr * sexpr * typ
-	| SObjCreate of string * sexpr list * typ
-	| SObjAccess of sexpr * sexpr * typ
+(*	| SObjCreate of string * sexpr list * typ
+	| SObjAccess of sexpr * sexpr * typ *)
 	(* Array access *)
 	| SCallExpr of sexpr * sexpr list * typ 
 	| SNoexpr
@@ -26,18 +26,18 @@ and sstmt =
   | SExprStmt of sexpr
   | SVarDecl of svdecl
   | SFunDecl of sfdecl
-  | SClassDecl of scdecl
+ (* | SClassDecl of scdecl *)
   | SReturn of sexpr
   | SIf of sexpr * sstmt * sstmt
   | SFor of sexpr * sexpr * sexpr * sstmt
   | SWhile of sexpr * sstmt
-
+(*
 and sfexpr = {
   sfeReturnType : typ;
   sfeFormals : sbind list;
   sfeBody: sstmt list;
 }
-
+*)
 and sfdecl = {
   sfdReturnType : typ;
   sfdFname : string;
@@ -46,12 +46,12 @@ and sfdecl = {
   sfdLocals : expr list;
   sfdBody : stmt list;
 }
-
+(*
 and scdecl = {
   scname : string;
   scproperties : svdecl list;
 }
-
+*)
 and sprogram = fdecl
 
 
