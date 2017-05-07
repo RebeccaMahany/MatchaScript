1f0b4c431ecf20f9e9e3f7880c7977a4af5e68b3
Jorbay <jao2154@columbia.edu>
2017-05-07 17:09:25 -0400

    Update fake_sast.ml
    added fake sast for assigning values to variables and adding variables

--- fake_sast.ml
+++ fake_sast.ml
@@ -10,3 +10,5 @@ let empty_prog : sprogram = []
 
 let create_fake_sast = 
 	empty_prog
+	
+let one_plus_one : sprogram = [SVarDecl(Int,"variable_b",IntLit(1)); SVarDecl(Int,"variable_a",IntLit(1)); SExprStmt(SBinop(SId("variable_a", Int), Add, SId("variable_b", Int), Int))]
